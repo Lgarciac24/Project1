6c79a0ca81f7fc99afcef62027428ebc8b593ee8
Lorena Garcia Cardona <lore@Lorenas-MacBook-Pro.local>
2022-04-07 17:49:41 -0500

    Lorena trabajo version 2

--- scripts.js
+++ scripts.js
@@ -14,26 +14,31 @@ let h1text = document.createElement("h1");
 h1text.innerText = "Hi Lorena, here's your progress!";
 div1.appendChild(h1text)
 
-let div11 = document.createElement("div")
-div11.className = "column"
+let div2 = document.createElement("div");
+div2.className = "div2"
+body.appendChild(div2)
+
+
+let div21 = document.createElement("div")
+div21.className = "column"
 let h2text1 = document.createElement("h2")
 h2text1.innerText = "Clase #1"
 
 let h3text1= document.createElement("h3");
 h3text1.innerText = "HTML y CSS"
-div11.appendChild(h2text1)
-div11.appendChild(h3text1)
-body.appendChild(div11)
+div21.appendChild(h2text1)
+div21.appendChild(h3text1)
+div2.appendChild(div21)
 
 
 h2text1.onclick = function (){
 
-    let div12 = document.createElement("div")
-    div12.className = "column"
+    let div22 = document.createElement("div")
+    div22.className = "column"
     let ptext = document.createElement("p")
     ptext.innerText="Esta es la segunda columna";
-    div12.appendChild(ptext)
-    body.appendChild(div12)
+    div22.appendChild(ptext)
+    div2.appendChild(div22)
 
 }
 

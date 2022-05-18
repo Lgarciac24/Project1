575180d14198f8ccb4b8cc1cec199aa95dbea9d5
Lorena Garcia Cardona <lore@Lorenas-MacBook-Pro.local>
2022-05-18 18:22:03 -0500

    Clase Mayo 18 2022

--- teacher/scripts.js
+++ teacher/scripts.js
@@ -2,76 +2,104 @@
 fetch('http://localhost:3000/users', {
 
 })
-.then(response => response.json())
-.then(data => {
-    console.log(data)   
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
 
-    // Loop to get all IDS
-    for (let i = 0; i < data.length; i++) {
-        const element = data[i].id;
-        
-        console.log(element)
-
-        // Creating the p tag for the text
-        let dataid = document.createElement("p");
-        dataid.innerText = element;
-        
-        let infoid = document.getElementById("info");
-        
-        // Adding it to the corresponding div
-        infoid.appendChild(dataid)
-
-    }
-
-    // Loop to get all names
-    for (let j = 0; j < data.length; j++) {
-        const elementname = data[j].name;
-
-        // Creating the p tag for the text
-        let datanames = document.createElement("p");
-        datanames.innerText = elementname;
-        
-        let infonames = document.getElementById("names");
-        
-         // Adding it to the corresponding div
-        infonames.appendChild(datanames)
-
-    }
-
-    // Loop to get all progress
-    for (let k = 0; k < data.length; k++) {
-        const elementprogress = data[k].sessionsGiven;
-
-
-        // Creating the p tag for the text
-        let dataprogr = document.createElement("p");
-        dataprogr.innerText = elementprogress;
-        
-        let infoprogr = document.getElementById("progresses");
-        
-         // Adding it to the corresponding div
-         infoprogr.appendChild(dataprogr)
-
-    }
-
-    // Loop for action buttons
-    for (let l = 0; l < data.length; l++) {
-        const elementbuttons = data[l];
-
-
-        // Creating button EDIT
-        let editbutton = document.createElement("button");
        
+       //Adding New Student
+
+       let addNewStudent = document.getElementById("addNewStudent");
+       addNewStudent.onclick = function()  {
+           
+            let tabla = document.getElementById("tabla");
+            tabla.style.display = "none";
+            formulario.style.display = "block";
+
+
+
+
+            let cerrar = document.getElementById("cerrar");
+            cerrar.onclick = function () {
+                tabla.style.display = "block";
+                formulario.style.display = "none";
+            }
+       }
+       
+       
+       
+        //Loop to get all IDS
+        for (let i = 0; i < data.length; i++) {
+            const studentId = data[i].id;
+            const elementname = data[i].name;
+            const elementprogress = data[i].progress;
+
+            console.log(studentId)
+
+            // Creating the p tag for the text
+            let dataid = document.createElement("p");
+            dataid.innerText = studentId;
+
+            let divId = document.getElementById("info");
+
+            // Adding it to the corresponding div
+            divId.appendChild(dataid)
+
+          
+
+            // Creating the p tag for the text
+            let datanames = document.createElement("p");
+            datanames.innerText = elementname;
+
+            let divnames = document.getElementById("names");
+
+            // Adding it to the corresponding div
+            divnames.appendChild(datanames)
+
+
+            // Creating the p tag for the text
+            let dataprogr = document.createElement("p");
+            dataprogr.innerText = elementprogress;
+
+            let divprogr = document.getElementById("progresses");
+
+            // Adding it to the corresponding div
+            divprogr.appendChild(dataprogr)
+
         
-        
-        let infoprogr = document.getElementById("progresses");
-        
-         // Adding it to the corresponding div
-         infoprogr.appendChild(dataprogr)
 
-    }
+            let divbutton = document.createElement("div");
+
+            // Creating button EDIT
+            let editbutton = document.createElement("button");
+            editbutton.innerText = "E"
+
+            editbutton.onclick = function () {
+  
+                let tabla = document.getElementById("tabla");
+                tabla.style.display = "none";
+                formulario.style.display = "block";
+    
+                let inputName = document.getElementById("inputName");
+                inputName.value = elementname;
+
+                let cerrar = document.getElementById("cerrar");
+                cerrar.onclick = function () {
+                    tabla.style.display = "block";
+                    formulario.style.display = "none";
+                    inputName.value = "";
+                }
+            }
 
 
+            // Adding it to the corresponding div
+            divprogr.appendChild(dataprogr)
 
+            let actions = document.getElementById("actions");
 
-})
+            actions.appendChild(divbutton);
+            divbutton.appendChild(editbutton)
+
+        }
+
+    })

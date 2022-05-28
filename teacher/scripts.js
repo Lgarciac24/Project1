94a6bf3f9d8d614ec4e5cacfa5a9b1a77d604f41
Lorena Garcia Cardona <lore@Lorenas-MacBook-Pro.local>
2022-05-28 15:27:44 -0500

    Updates: Button Edit and Pop up div, button D and alert pop up

--- teacher/scripts.js
+++ teacher/scripts.js
@@ -42,7 +42,7 @@ fetch('http://localhost:3000/users', {
             fetch("http://localhost:3000/users", {
                 method: "POST",
                 headers: {
-                    'Content-Type': 'application/json'
+
                 },
                 body: JSON.stringify({
 
@@ -99,36 +99,136 @@ fetch('http://localhost:3000/users', {
             let divbutton = document.createElement("div");
 
             // Creating button EDIT
-            let editbutton = document.createElement("button");
-            editbutton.innerText = "E"
+            let editButton = document.createElement("button");
+            editButton.innerText = "E"
 
-            editbutton.onclick = function () {
+            // Getting the div
+            let student = document.getElementById("myStudent");
 
-                // Getting the div
-                let student = document.getElementById("myStudent");
+            // Getting the X that closes form
+            let span2 = document.getElementsByClassName("close2")[0];
 
-                // Getting the X that closes form
-                let span = document.getElementsByClassName("close")[0];
+            //Funtion for the X 
+            span2.onclick = function () {
+                student.style.display = "none";
 
-                let inputName = document.getElementById("inputName");
-                inputName.value = elementname;
 
-                let cerrar = document.getElementById("cerrar");
-                cerrar.onclick = function () {
-                    tabla.style.display = "block";
-                    formulario.style.display = "none";
-                    inputName.value = "";
+            }
+
+            //showing name from the data.json --NO FUNCIONA
+            let inputName2 = document.getElementById("inputName2");
+            inputName2.value = elementname;
+
+            //Action when click E
+            editButton.onclick = function () {
+
+                // showing EDIT student info
+                student.style.display = "block";
+
+                // When submitChanges is clicked
+                let submitChanges = document.getElementById("submitChanges");
+
+                submitChanges.onclick = function () {
+                    fetch("http://localhost:3000/users", {
+                        method: "PUT",
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+
+                            // PREGUNTA PARA SEBASTIAN
+
+                        })
+                    }).then(r => {
+                        console.log(r)
+                    })
+
                 }
+
+
+            }
+
+            // To close window
+            window.onclick = function (event) {
+                if (event.target == student) {
+                    student.style.display = "none";
+                }
+
             }
 
 
-            // Adding it to the corresponding div
+            // Adding it to the corresponding div - SHOWING UNDEFINED - ASK S
             divprogr.appendChild(dataprogr)
+
+            // Creating button DELETE
+            let deleteButton = document.createElement("button");
+            deleteButton.innerText = "D"
 
             let actions = document.getElementById("actions");
 
             actions.appendChild(divbutton);
-            divbutton.appendChild(editbutton)
+            divbutton.appendChild(editButton);
+            divbutton.appendChild(deleteButton);
+
+            // Getting the first DIV
+            let alertDelete = document.getElementById("alertDelete");
+
+            // Getting the X that closes the alert
+            let close3 = document.getElementsByClassName("close3")[0];
+
+            // Function to close alert
+            close3.onclick = function () {
+
+                alertDelete.style.display = "none";
+
+            }
+
+            // When clicked D
+            deleteButton.onclick = function () {
+                alertDelete.style.display = "block";
+
+                //Question -- PREGUNTAR S -- DESDE EL SEGUNDO D EMPIEZA A SUMAR LOS NOMRBES
+
+                let textoAlerta = document.createElement("h4");
+                textoAlerta.innerText = "Estas seguro que deseas eliminar " + elementname + "?";
+
+                let divAlert = document.getElementById("questionAlert");
+
+                divAlert.appendChild(textoAlerta);
+
+                //function when Yes
+
+                //Getting YES button
+
+                let deleteButton = document.getElementById("deleteButton");
+
+                // When YES is clicked
+                deleteButton.onclick = function () {
+
+                    fetch("http://localhost:3000/clases", {
+                        method: "DELETE",
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: ""
+                    }).then(r => {
+                        console.log(r)
+                    });
+
+                }
+                //Getting NO button
+                cancelButton = document.getElementById("cancelButton");
+
+                // When NO is clicked
+                cancelButton.onclick = function () {
+                    alertDelete.style.display = "none";
+
+                }
+
+
+            }
+
+
 
         }
 

ac7d505673b660a2031f07d3542c3ae6cbcffa53
Lorena Garcia Cardona <lore@Lorenas-MacBook-Pro.local>
2022-06-06 19:27:56 -0500

    camel case - clases table

--- teacher/scripts.js
+++ teacher/scripts.js
@@ -1,5 +1,4 @@
-// All access to DOM needs to be assigned to a const variable
-
+// All access to DOM needs to be assigned to a const variable - done
 
 // To do: Actualizar para que tambien se guarde el progress
 
@@ -24,6 +23,9 @@ const inputName = document.getElementById('inputName');
 
 // Value from form email
 const inputEmail = document.getElementById('inputEmail');
+
+// Value from form progress
+const inputProgr = document.getElementById('inputProgr');
 
 // Function for the button
 addNewStudent.onclick = function () {
@@ -51,6 +53,7 @@ submitNew.onclick = function () {
     body: JSON.stringify({
       name: inputName.value,
       email: inputEmail.value,
+      progress: inputProgr.value,
     }),
   })
     .then((r) => {
@@ -70,47 +73,56 @@ fetch('http://localhost:3000/users', {})
     //Loop to get all IDS
     for (let i = 0; i < data.length; i++) {
       const studentId = data[i].id;
-      const elementname = data[i].name;
-      const elementemail = data[i].email;
-      const elementprogress = data[i].progress;
-
-      console.log(studentId);
+      const elementName = data[i].name;
+      const elementEmail = data[i].email;
+      const elementProgress = data[i].progress;
 
       // Creating the p tag for the text
-      let dataid = document.createElement('p');
-      dataid.innerText = studentId;
+      const dataId = document.createElement('p');
+      dataId.innerText = studentId;
 
-      let divId = document.getElementById('info');
+      const divId = document.getElementById('info');
 
       // Adding it to the corresponding div
-      divId.appendChild(dataid);
+      divId.appendChild(dataId);
 
       // Creating the p tag for the text
-      let datanames = document.createElement('p');
-      datanames.innerText = elementname;
+      const dataNames = document.createElement('p');
+      dataNames.innerText = elementName;
 
-      let divnames = document.getElementById('names');
+      const divNames = document.getElementById('names');
 
       // Adding it to the corresponding div
-      divnames.appendChild(datanames);
+      divNames.appendChild(dataNames);
+
+      // Creating the p tag for email text
+      const dataEmail = document.createElement('p');
+      dataEmail.innerText = elementEmail;
+
+      const divEmails = document.getElementById('emails');
+
+      // Adding it to the corresponding div
+      divEmails.appendChild(dataEmail);
+
 
       // Creating the p tag for the text
-      let dataprogr = document.createElement('p');
-      dataprogr.innerText = elementprogress;
+      const dataProgr = document.createElement('p');
+      dataProgr.innerText = elementProgress;
 
-      let divprogr = document.getElementById('progresses');
+      const divProgr = document.getElementById('progresses');
 
       // Adding it to the corresponding div
-      divprogr.appendChild(dataprogr);
+      divProgr.appendChild(dataProgr);
 
-      let divbutton = document.createElement('div');
+      const divButton = document.createElement('div');
 
       // Creating button EDIT
-      let editButton = document.createElement('button');
+      const editButton = document.createElement('button');
       editButton.innerText = 'E';
       editButton.id = studentId;
-      editButton.setAttribute('studentName', elementname);
-      editButton.setAttribute('studentEmail', elementemail);
+      editButton.setAttribute('studentName', elementName);
+      editButton.setAttribute('studentEmail', elementEmail);
+      editButton.setAttribute('studentProgr', elementProgress);
 
       // Getting the div
       let student = document.getElementById('myStudent');
@@ -128,12 +140,15 @@ fetch('http://localhost:3000/users', {})
         // showing EDIT student info
         student.style.display = 'block';
 
-        //showing name from the data.json --NO FUNCIONA
-        let inputName2 = document.getElementById('inputName2');
+        //showing name from the data.json 
+        const inputName2 = document.getElementById('inputName2');
         inputName2.value = editButton.getAttribute('studentName');
 
-        let inputEmail2 = document.getElementById('inputEmail2');
+        const inputEmail2 = document.getElementById('inputEmail2');
         inputEmail2.value = editButton.getAttribute('studentEmail');
+
+        const inputProgr2 = document.getElementById('inputProgr2');
+        inputProgr2.value = editButton.getAttribute('studentProgr');
 
         // When submitChanges is clicked
         let submitChanges = document.getElementById('submitChanges');
@@ -147,6 +162,7 @@ fetch('http://localhost:3000/users', {})
             body: JSON.stringify({
               name: inputName2.value,
               email: inputEmail2.value,
+              progress: inputProgr2.value,
             }),
           })
             .then((r) => {
@@ -167,24 +183,24 @@ fetch('http://localhost:3000/users', {})
         }
       };
 
-      // Adding it to the corresponding div - SHOWING UNDEFINED - ASK S
-      divprogr.appendChild(dataprogr);
+      // Adding it to the corresponding div
+      divProgr.appendChild(dataProgr);
 
       // Creating button DELETE
-      let deleteButton = document.createElement('button');
+      const deleteButton = document.createElement('button');
       deleteButton.innerText = 'D';
 
-      let actions = document.getElementById('actions');
+      const actions = document.getElementById('actions');
 
-      actions.appendChild(divbutton);
-      divbutton.appendChild(editButton);
-      divbutton.appendChild(deleteButton);
+      actions.appendChild(divButton);
+      divButton.appendChild(editButton);
+      divButton.appendChild(deleteButton);
 
       // Getting the first DIV
-      let alertDelete = document.getElementById('alertDelete');
+      const alertDelete = document.getElementById('alertDelete');
 
       // Getting the X that closes the alert
-      let close3 = document.getElementsByClassName('close3')[0];
+      const close3 = document.getElementsByClassName('close3')[0];
 
       // Function to close alert
       close3.onclick = function () {
@@ -197,11 +213,11 @@ fetch('http://localhost:3000/users', {})
 
         //Question -- PREGUNTAR S -- DESDE EL SEGUNDO D EMPIEZA A SUMAR LOS NOMRBES
 
-        let textoAlerta = document.createElement('h4');
+        const textoAlerta = document.createElement('h4');
         textoAlerta.innerText =
-          'Estas seguro que deseas eliminar ' + elementname + '?';
-
-        let divAlert = document.getElementById('questionAlert');
+          'Estas seguro que deseas eliminar ' + elementName + '?';
+ 
+        const divAlert = document.getElementById('questionAlert');
 
         divAlert.appendChild(textoAlerta);
 
@@ -240,3 +256,64 @@ fetch('http://localhost:3000/users', {})
       };
     }
   });
+
+fetch('http://localhost:3000/clases', {})
+.then((response) => response.json())
+.then ((data) => {
+    for (let i = 0; i < data.length; i++) {
+        const classNumber = data[i].title;
+        const classTopic  = data[i].subtitle;
+        const classTitle = data[i].mainTitle;
+        const classDescription = data[i].introText;
+        const classHomew = data[i].homework;
+        const classlinks = data[i].links;
+
+     // Creating the p tag for the text for Class #
+      const dataClassNumber = document.createElement('p');
+      dataClassNumber.innerText = classNumber;
+
+      const divClassNumber = document.getElementById('classnumber');
+
+      // Adding it to the corresponding div
+      divClassNumber.appendChild(dataClassNumber);
+
+      // Creating the p tag for the text for Topic
+      const dataTopic = document.createElement('p');
+      dataTopic.innerText = classTopic;
+
+      const divClassTopic = document.getElementById('classtopic');
+
+      // Adding it to the corresponding div
+      divClassTopic.appendChild(dataTopic);
+
+      // Creating the p tag for the text for Title
+      const dataTitle = document.createElement('p');
+      dataTitle.innerText = classTitle;
+
+      const divClassTitle = document.getElementById('classtitle');
+
+      // Adding it to the corresponding div
+      divClassTitle.appendChild(dataTitle);
+
+      // Creating the p tag for the text for Descrip
+      const dataDescr = document.createElement('p');
+      dataDescr.innerText = classDescription;
+
+      const divClassDescr = document.getElementById('classdescr');
+
+      // Adding it to the corresponding div
+      divClassDescr.appendChild(dataDescr);
+
+      // Creating the p tag for the text for Progr
+      const dataHomew = document.createElement('p');
+      dataHomew.innerText = classHomew;
+
+      const divClassHomew = document.getElementById('classhomew');
+
+      // Adding it to the corresponding div
+      divClassHomew.appendChild(dataHomew);
+    
+
+    }
+
+});
